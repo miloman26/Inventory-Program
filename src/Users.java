edcab0270b1a56b272c86b31c6ec7d69c6148a4c
miloman26 <milothompson@rocketmail.com>
2013-07-12 04:21:39 -0400

    Cant get the form fields to pass values
    trying to get NewJFrame.form jTextField to pass Value to framecust.form
    with getter setter being Users.java

---
+++ src/Users.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author milo
+ */
+
+public class Users {
+    protected String Musername;
+    NewJFrame Obj2= new NewJFrame();
+    
+   public void setUsername(String pUsername){
+       Musername = Obj2.jTextField2.getText();
+   } 
+   public String getUsername(){
+   return Musername;}
+}
